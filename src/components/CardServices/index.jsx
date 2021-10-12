735de1792becbe648fc968af1f252faffc71862d
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-10-12 14:12:35 -0300

    Adicionada animação nos cards de categorias e serviços ao entrar na página

--- src/components/CardServices/index.jsx
+++ src/components/CardServices/index.jsx
@@ -1,21 +1,40 @@
 import '../../styles/services.scss';
 
-import React from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import React, { useEffect } from 'react';
 
 import Card from './Card';
 
 const Services = ({ services }) => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.set((index) => ({
+      opacity: 0,
+      x: -200,
+      scale: 1.3,
+      transition: { delay: index * 0.3, ease: 'anticipate' },
+    }));
+    controls.start((index) => ({
+      opacity: 1,
+      x: 0,
+      scale: 1,
+      transition: { delay: index * 0.3 },
+    }));
+  }, [controls]);
+
   return (
     <div className="main-page__content__services" data-testid="service-cards">
       {services.map((item, index) => (
-        <Card
-          icon={item.icon}
-          price={item.price}
-          serviceName={item.serviceName}
-          serviceDescription={item.description}
-          id={item.id}
-          key={index}
-        />
+        <motion.div key={index} custom={index} animate={controls}>
+          <Card
+            icon={item.icon}
+            price={item.price}
+            serviceName={item.serviceName}
+            serviceDescription={item.description}
+            id={item.id}
+          />
+        </motion.div>
       ))}
     </div>
   );
--- src/components/SelectCategory/index.jsx
+++ src/components/SelectCategory/index.jsx
@@ -1,22 +1,41 @@
 import '../../styles/categories.scss';
 
-import React from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import React, { useEffect } from 'react';
 
 import CategoryCard from './CategoryCard';
 
 const SelectCategory = ({ categories, onSelectCard }) => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.set((index) => ({
+      opacity: 0,
+      x: -200,
+      scale: 1.3,
+      transition: { delay: index * 0.3, ease: 'anticipate' },
+    }));
+    controls.start((index) => ({
+      opacity: 1,
+      x: 0,
+      scale: 1,
+      transition: { delay: index * 0.3 },
+    }));
+  }, [controls]);
+
   return (
     <div className="main-page__content__categories" data-testid="category-card">
       {categories.map((item, index) => (
-        <CategoryCard
-          name={item.name}
-          icon={item.icon}
-          focusClass={item.focusClass}
-          notFocusClass={item.notFocusClass}
-          value={item.name}
-          key={index}
-          onSelectCard={onSelectCard}
-        />
+        <motion.div key={index} custom={index} animate={controls}>
+          <CategoryCard
+            name={item.name}
+            icon={item.icon}
+            focusClass={item.focusClass}
+            notFocusClass={item.notFocusClass}
+            value={item.name}
+            onSelectCard={onSelectCard}
+          />
+        </motion.div>
       ))}
     </div>
   );
